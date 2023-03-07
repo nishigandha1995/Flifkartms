5cf1231534123594cd0bed811374a00c61f1205c
nishi kutemate <nishik@gmail.com>
2023-03-08 01:35:36 +0530

    hello everyone

---
+++ src/main/java/com/example/Flipkartms/FlipkartmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Flipkartms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartmsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Flipkartms/MenCloths.java
@@ -0,0 +1,13 @@
+package com.example.Flipkartms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MenCloths {
+    @GetMapping("/nishi")
+    public String getData(){
+        return "Please book your  Flight ticket, Choose Your Fav Airlines Idigo";
+    }
+
+}
